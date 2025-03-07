e03aa635c1176e7f7383116852539a46b97c0b3a
eddy lee <192184280+Yongji-Li@users.noreply.github.com>
2025-03-07 18:46:43 +0900

    v2

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
+  console.log('access');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello CTI!');
 });
 
-server.listen(8080, () => {
-  console.log('Server is running at http://localhost:8080');
+server.listen(8081, () => {
+  console.log('Server is running at http://localhost:8081');
 });
