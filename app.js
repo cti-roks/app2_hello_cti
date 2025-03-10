e2c051bf7e416321cf3c6c9fb2bb9b789df551cf
Yongji-Li <192184280+Yongji-Li@users.noreply.github.com>
2025-03-10 16:53:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('access');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-<<<<<<< HEAD
-  res.end('Hello CTI!');
-=======
-  res.end('Hello CTI!-v3');
->>>>>>> 8289e2baba039707e0f26df48cfb69aced6786d7
+  res.end('Hello! cti');
 });
 
 server.listen(8081, () => {
